e8dca6e643e52a7c0659bfc927a1792b2466e5d0
Jaap Karssenberg <pardus@cpan.org>
2008-11-27 20:40:20 +0100

    Imported xdot.py from http://jrfonseca.googlecode.com/svn/trunk/xdot/

---
+++ zim/_lib/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf8 -*-
+
+# Copyright 2008 Jaap Karssenberg <pardus@cpan.org>
+
+'''This package contains third party libraries to be packaged
+with zim. See the respective files for the copyright and license
+notices.
+'''
