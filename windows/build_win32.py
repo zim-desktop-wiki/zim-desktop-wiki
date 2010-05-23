377d1e041070f99b9ab2bf9694242fed55394762
Brendan Kidwell <snarf@glump.net>
2010-05-23 19:14:12 -0400

    Merged Jaap's patch from Bug 567614: * Moved py2exe calls to setup.py so build dependencies are called properly * Changed build_win32.py to call setup.py * Moved target directory from "windows/zim" to "windows/build" -> probably needs change in NSIS script as well * Added translation files in share directory

--- windows/build_win32.py
+++ windows/build_win32.py
@@ -1,32 +1,30 @@
-import os, sys, shutil
-import py2exe
+import os
+import shutil
 import datetime
-from distutils.core import setup
-sys.path.append(".")
+import subprocess
+
 from zim import __version__
 
 # For the complete Windows build procedure, please read
 # README-BUILD-win32.txt
 
-# If run without args, build executables, in quiet mode.
-if len(sys.argv) == 1:
-    sys.argv.append("py2exe")
+#  Clean up the build directory
+shutil.rmtree("windows/build", True)
 
 # update "data" folder
-shutil.rmtree("windows/zim/data", True)
-shutil.copytree("data", "windows/zim/data")
+shutil.copytree("data", "windows/build/data")
 
 # If you installed GTK to a different folder, change these lines:
-shutil.rmtree("windows/zim/etc", True)
-shutil.copytree("c:/Program Files/Common Files/GTK/2.0/etc", "windows/zim/etc")
-shutil.rmtree("windows/zim/lib", True)
-shutil.copytree("c:/Program Files/Common Files/GTK/2.0/lib", "windows/zim/lib")
-shutil.rmtree("windows/zim/share", True)
-shutil.copytree("c:/Program Files/Common Files/GTK/2.0/share", "windows/zim/share")
+shutil.copytree("c:/Program Files/Common Files/GTK/2.0/etc", "windows/build/etc")
+shutil.copytree("c:/Program Files/Common Files/GTK/2.0/lib", "windows/build/lib")
+shutil.copytree("c:/Program Files/Common Files/GTK/2.0/share", "windows/build/share")
+
+# Copy translation files
+shutil.copytree("locale", "windows/build/share/locale")
 
 # copy plugins folder so Preferences dialog can iterate through them
-shutil.rmtree("windows/zim/zim", True)
-shutil.copytree("zim/plugins", "windows/zim/zim/plugins")
+shutil.rmtree("windows/build/zim", True)
+shutil.copytree("zim/plugins", "windows/build/zim/plugins")
 
 # print out version number
 f = open("windows/version-and-date.nsi", "w")
@@ -34,22 +32,9 @@ f.write('!define VER "%s"\n' % __version__)
 f.write('!define BUILDDATE "%s"\n' % datetime.datetime.now().strftime("%Y-%m-%d"))
 f.close()
 
+# create main.exe
+subprocess.check_call(['setup.py', 'py2exe'])
+
 # NSIS script compiles to "dist" folder but compiler won't create it if it's needed
 if not os.path.exists("dist"):
     os.mkdir("dist")
-
-# create main.exe
-setup(
-    options = {"py2exe": {"compressed": 0,
-                          "optimize": 2,
-                          "ascii": 1,
-                          "bundle_files": 3,
-                          "packages": ["encodings", "cairo", "atk", "pangocairo", "zim", "bzrlib"],
-                          "dist_dir" : "windows/zim"
-                          }},
-    zipfile = None,
-    windows = [{
-            "script": "zim.py",
-            "icon_resources": [(1, "data/pixmaps/favicon.ico")]
-        }],
-)
