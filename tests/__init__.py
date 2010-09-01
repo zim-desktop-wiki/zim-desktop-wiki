596db038df9f577a1fa8f730c68bd6736b026ec8
Jaap Karssenberg <jaap@no-spoon>
2010-08-31 22:54:17 -0400

    Added new plugin for inline evaluation of expressions

--- tests/__init__.py
+++ tests/__init__.py
@@ -19,7 +19,7 @@ __all__ = [
 	'history', 'plugins',
 	'export', 'www', 'search',
 	'pageview',
-	'calendar', 'printtobrowser', 'versioncontrol',
+	'calendar', 'printtobrowser', 'versioncontrol', 'inlinecalculator',
 	'equationeditor', 'diagrameditor',
 ]
 
