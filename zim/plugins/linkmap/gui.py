05269155c657253e578b99e0977e476ca284171b
Jaap Karssenberg <pardus@cpan.org>
2011-04-14 22:24:09 +0200

    Fixed linkmap widget to actually go to the clicked page

--- zim/plugins/linkmap/gui.py
+++ zim/plugins/linkmap/gui.py
@@ -5,6 +5,8 @@
 import gtk
 
 from zim._lib import xdot
+
+from zim.notebook import Path
 from zim.gui.widgets import ui_environment, Dialog, IconButton
 
 from zim.plugins.linkmap import LinkMap
@@ -75,5 +77,5 @@ class LinkMapDialog(Dialog):
 			button.connect('clicked', method)
 			vbox.pack_start(button, False)
 
-	def on_node_clicked(self, *a):
-		print a
+	def on_node_clicked(self, widget, name, event):
+		self.ui.open_page(Path(name))
