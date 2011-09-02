77c133c2eb70661369895a2a848151fffa734241
Jaap Karssenberg <pardus@cpan.org>
2011-09-02 19:24:44 +0200

    Made current page highlight bold in the index and tag index

--- zim/plugins/tags.py
+++ zim/plugins/tags.py
@@ -12,7 +12,7 @@ import logging
 
 from zim.plugins import PluginClass
 from zim.gui.pageindex import PageTreeStore, PageTreeIter, PageTreeView, \
-	NAME_COL, PATH_COL, EMPTY_COL, STYLE_COL, FGCOLOR_COL, N_CHILD_COL
+	NAME_COL, PATH_COL, EMPTY_COL, STYLE_COL, FGCOLOR_COL, WEIGHT_COL, N_CHILD_COL
 from zim.notebook import Path
 from zim.index import IndexPath, IndexTag
 from zim.gui.widgets import LEFT_PANE
@@ -46,8 +46,18 @@ class DuplicatePageTreeStore(PageTreeStore):
 	multiple times in the tree.
 	'''
 
+	def select_page(self, path):
+		oldpath = self.selected_page
+		self.selected_page = path
+
+		for mypath in (oldpath, path):
+			if mypath:	
+				for treepath in self.get_treepaths(mypath):
+					treeiter = self.get_iter(treepath)
+					self.emit('row-changed', treepath, treeiter)
+
 	def get_treepath(self, path):
-		'''Just returns the first treepath matching notebook path 'path' '''
+		# Just returns the first treepath matching notebook path
 		treepaths = self.get_treepaths(path)
 		if treepaths:
 			return treepaths[0]
@@ -366,6 +376,9 @@ class TagsPageTreeStore(DuplicatePageTreeStore):
 					return self.EMPTY_COLOR
 				else:
 					return self.NORMAL_COLOR
+			elif column == WEIGHT_COL:
+				return pango.WEIGHT_NORMAL
+				# TODO: use this property to show tags in current page?
 			elif column == N_CHILD_COL:
 				return ''
 				## Due to multiple tag filtering this result is no good..
@@ -560,6 +573,7 @@ class TagsPageTreeView(PageTreeView):
 		filtermodel.get_treepath = get_treepath
 		filtermodel.get_treepaths = get_treepaths
 		filtermodel.index = model.index
+		filtermodel.select_page = model.select_page
 
 		PageTreeView.set_model(self, filtermodel)
 
@@ -738,8 +752,13 @@ class TagsPluginWidget(gtk.VPaned):
 		else:
 			self.plugin.ui.connect_after('open-notebook', lambda *a: self.reload_model())
 
+		self.plugin.ui.connect('open-page', self.on_open_page)
 		self.plugin.ui.connect('start-index-update', lambda o: self.disconnect_model())
 		self.plugin.ui.connect('end-index-update', lambda o: self.reload_model())
+
+
+	def on_open_page(self, ui, page, path):
+		self.treeview.select_page(path)
 
 	def toggle_treeview(self):
 		'''Toggle the treeview type in the widget'''
@@ -789,6 +808,9 @@ class TagsPluginWidget(gtk.VPaned):
 			model = TagsPageTreeStore(self.plugin.ui.notebook.index) # FIXME clean up law of D
 
 		self.treeview.set_model(model)
+
+		if self.plugin.ui.page:
+			model.select_page(self.plugin.ui.page)
 
 
 class TagsPlugin(PluginClass):
