db9c33cb3f1281fcd1312d37b6862167f70f5946
zim <>
2012-02-20 17:59:43 +0100

    report John Drinkwater git test cases

--- tests/versioncontrol.py
+++ tests/versioncontrol.py
@@ -12,9 +12,11 @@ from zim.plugins.versioncontrol import VersionControlPlugin, NoChangesError
 
 from zim.plugins.versioncontrol.bzr import BazaarVCS
 from zim.plugins.versioncontrol.hg import MercurialVCS
+from zim.plugins.versioncontrol.git import GitVCS
 
 import zim.plugins.versioncontrol.bzr
 import zim.plugins.versioncontrol.hg
+import zim.plugins.versioncontrol.git
 
 
 # We define our own tmp dir here instead of using tests.create_tmp_dir
@@ -35,6 +37,11 @@ def get_tmp_dir(name):
 	return dir
 
 
+#####################################################
+#
+# BAZAAR BACKEND TEST
+#
+#####################################################
 @tests.slowTest
 @tests.skipUnless(BazaarVCS.check_dependencies(), 'Missing dependencies')
 class TestBazaar(tests.TestCase):
@@ -155,6 +162,198 @@ bar
 ''' )
 
 
+
+#####################################################
+#
+# GIT BACKEND TEST
+#
+#####################################################
+@tests.slowTest
+@tests.skipUnless(GitVCS.check_dependencies(), 'Missing dependencies')
+class TestGit(tests.TestCase):
+
+	def setUp(self):
+		zim.plugins.versioncontrol.generic.TEST_MODE = False
+
+	def tearDown(self):
+		zim.plugins.versioncontrol.generic.TEST_MODE = True
+
+	def runTest(self):
+		'''Test Git version control'''
+		print '\n!! Some raw output from Git could appear here !!'
+
+		root = get_tmp_dir('versioncontrol_TestGit')
+		vcs = GitVCS(root)
+		vcs.init()
+
+		#~ for notebookdir in (root, root.subdir('foobar')):
+			#~ detected = VersionControlPlugin._detect_vcs(notebookdir)
+			#~ self.assertEqual(detected.__class__, BazaarVCS)
+			#~ del detected # don't keep multiple instances around
+
+		subdir = root.subdir('foo/bar')
+		file = subdir.file('baz.txt')
+		file.write('foo\nbar\n')
+		self.assertEqual(''.join(vcs.get_status()), '''\
+# On branch master
+#
+# Initial commit
+#
+# Changes to be committed:
+#   (use "git rm --cached <file>..." to unstage)
+#
+#	new file:   .gitignore
+#	new file:   foo/bar/baz.txt
+#
+''' )
+
+		vcs.update_staging()
+		vcs.commit('test 1')
+#[master 0f4132e] test 1
+# 1 files changed, 3 insertions(+), 0 deletions(-)
+# create mode 100644 foo/bar/baz.txt
+
+		# git plugin doesnt support this atm
+		#self.assertRaises(NoChangesError, vcs.commit, 'test 1')
+
+		file = subdir.file('bar.txt')
+		file.write('second\ntest\n')
+
+		self.assertEqual(''.join(vcs.get_status()), '''\
+# On branch master
+# Changes to be committed:
+#   (use "git reset HEAD <file>..." to unstage)
+#
+#	new file:   foo/bar/bar.txt
+#
+''' )
+
+		vcs.update_staging()
+		vcs.commit('test 2')
+#[master dbebdf1] test 2
+# 0 files changed, 0 insertions(+), 0 deletions(-)
+# create mode 100644 foo/bar/bar.txt
+
+		# git plugin doesnt support this atm
+		#self.assertRaises(NoChangesError, vcs.commit, 'test 2')
+
+		# these lines contain file perms & hashes
+		ignorelines = lambda line: not (line.startswith('new') or line.startswith('index'))
+		diff = vcs.get_diff(versions=('HEAD'))
+# john@joran:~/code/zim/TEST$ git diff master^
+# diff --git a/foo/bar/bar.txt b/foo/bar/bar.txt
+# new file mode 100644
+# index 0000000..e69de29
+		diff = ''.join(filter(ignorelines, diff))
+		self.assertEqual(diff, '''\
+diff --git a/foo/bar/bar.txt b/foo/bar/bar.txt
+--- /dev/null
++++ b/foo/bar/bar.txt
+@@ -0,0 +1,2 @@
++second
++test
+''' )
+
+		file.write('second\nbaz\n')
+		diff = vcs.get_diff()
+		diff = ''.join(filter(ignorelines, diff))
+		self.assertEqual(diff, '''\
+diff --git a/foo/bar/bar.txt b/foo/bar/bar.txt
+--- a/foo/bar/bar.txt
++++ b/foo/bar/bar.txt
+@@ -1,2 +1,2 @@
+ second
+-test
++baz
+''' )
+
+		vcs.revert()
+		self.assertEqual(vcs.get_status(), [
+'# On branch master\n',
+'nothing to commit (working directory clean)\n'
+] )
+
+		file.write('second\nbaz\n')
+		vcs.commit_async('test 3')
+		diff = vcs.get_diff(versions=('HEAD', 'HEAD^'))
+		diff = ''.join(filter(ignorelines, diff))
+		self.assertEqual(diff, '''\
+diff --git a/foo/bar/bar.txt b/foo/bar/bar.txt
+--- a/foo/bar/bar.txt
++++ b/foo/bar/bar.txt
+@@ -1,2 +1,2 @@
+ second
+-test
++baz
+''' )
+
+		versions = vcs.list_versions()
+
+		self.assertTrue(isinstance(versions,list))
+		#~ print 'VERSIONS>>', versions
+		print "VERSIONS", versions
+		self.assertTrue(len(versions) == 3)
+		self.assertTrue(isinstance(versions[0],tuple))
+		self.assertTrue(len(versions[0]) == 4)
+		self.assertTrue(isinstance(versions[0][0],str))
+		self.assertTrue(isinstance(versions[0][1],str))
+		self.assertTrue(isinstance(versions[0][2],str))
+		self.assertTrue(isinstance(versions[0][3],unicode))
+		self.assertEqual(versions[0][3], u'test 1\n')
+		self.assertTrue(len(versions[1]) == 4)
+		self.assertEqual(versions[1][3], u'test 2\n')
+		self.assertTrue(len(versions[2]) == 4)
+		self.assertEqual(versions[2][3], u'test 3\n')
+
+		# slightly different, we check the 2nd file
+		lines = vcs.get_version(file, version='HEAD^')
+		self.assertEqual(''.join(lines), '''\
+second
+test
+''' )
+
+#john@joran:/tmp/test_versioncontrol/versioncontrol_TestGit$ git annotate -t foo/bar/bar.txt
+#09be0483        (John Drinkwater        1309533637 +0100        1)second
+#526fb2b5        (John Drinkwater        1309533637 +0100        2)baz
+#john@joran:/tmp/test_versioncontrol/versioncontrol_TestGit$ git blame -s foo/bar/bar.txt
+#09be0483 1) second
+#526fb2b5 2) baz
+
+		annotated = vcs.get_annotated(file)
+		lines = []
+		for line in annotated:
+			# get rid of commit hash, its unique
+			commit, num, text = line.split(' ')
+			lines.append(num+' '+text)
+		self.assertEqual(''.join(lines), '''\
+1) second
+2) baz
+''' )
+		"""FIXME
+		"""
+
+# XXX ignore renames and deletions?
+
+# Below is a test that we dont need to handle, as we can be quite ignorant of them. Especially considering
+# how git tracks file moves, ie, it doesnt.
+
+#		file.rename(root.file('bar.txt'))
+#		diff = vcs.get_diff()
+#john@joran:~/code/zim/TEST$ git diff
+#diff --git a/foo/bar/bar.txt b/foo/bar/bar.txt
+#deleted file mode 100644
+#…
+
+#john@joran:~/code/zim/TEST$ git commit -a -m "Moved test 4"
+#[master b099d98] Moved test 4
+# 1 files changed, 0 insertions(+), 0 deletions(-)
+# rename foo/bar/{bar.txt => boo.txt} (100%)
+
+#####################################################
+#
+# MERCURIAL BACKEND TEST
+#
+#####################################################
 @tests.slowTest
 @tests.skipUnless(MercurialVCS.check_dependencies(), 'Missing dependencies')
 class TestMercurial(tests.TestCase):
@@ -166,8 +365,8 @@ class TestMercurial(tests.TestCase):
 		zim.plugins.versioncontrol.generic.TEST_MODE = True
 
 	def runTest(self):
-		'''Test Bazaar version control'''
-		print '\n!! Some raw output from Bazaar expected here !!'
+		'''Test Mercurial version control'''
+		print '\n!! Some raw output from Mercurial expected here !!'
 
 		root = get_tmp_dir('versioncontrol_TestMercurial')
 		vcs = MercurialVCS(root)
@@ -209,7 +408,7 @@ diff --git a/foo/bar/baz.txt b/foo/bar/baz.txt
 
 		
 		file.write('foo\nbaz\n')
-		vcs.commit_async('test 2') # The async operation is the same for BZR and HG, so we do not retest commit_async
+		vcs.commit_async('test 2')
 		diff = vcs.get_diff(versions=(0, 1))
 		diff = ''.join(filter(ignorelines, diff))
 		self.assertEqual(diff, '''\
@@ -224,10 +423,10 @@ diff --git a/foo/bar/baz.txt b/foo/bar/baz.txt
 		#~ print 'VERSIONS>>', versions
 		self.assertTrue(len(versions) == 2)
 		self.assertTrue(len(versions[0]) == 4)
-		self.assertEqual(versions[0][0], 0)
+		self.assertEqual(versions[0][0], str(0))
 		self.assertEqual(versions[0][3], u'test 1\n')
 		self.assertTrue(len(versions[1]) == 4)
-		self.assertEqual(versions[1][0], 1)
+		self.assertEqual(versions[1][0], str(1))
 		self.assertEqual(versions[1][3], u'test 2\n')
 
 		
@@ -258,6 +457,4 @@ diff --git a/foo/bar/baz.txt b/bar.txt
 rename from foo/bar/baz.txt
 rename to bar.txt
 ''' )
-
-
 
