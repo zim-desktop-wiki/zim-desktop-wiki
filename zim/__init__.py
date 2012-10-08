e06d8cd2cc010e14bea8b8a5bc70b430cced6b52
Jaap Karssenberg <jaap.karssenberg@gmail.com>
2012-10-08 21:17:51 +0200

    Release 0.57

--- zim/__init__.py
+++ zim/__init__.py
@@ -94,7 +94,7 @@ For asynchronous actions see L{zim.async}.
 
 
 # Bunch of meta data, used at least in the about dialog
-__version__ = '0.56'
+__version__ = '0.57'
 __url__='http://www.zim-wiki.org'
 __author__ = 'Jaap Karssenberg <jaap.karssenberg@gmail.com>'
 __copyright__ = 'Copyright 2008 - 2012 Jaap Karssenberg <jaap.karssenberg@gmail.com>'
