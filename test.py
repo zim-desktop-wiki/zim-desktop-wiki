58adb6a8b9abfee524322d77cef56672d90ac3c9
Jaap Karssenberg <jaap.karssenberg@gmail.com>
2016-10-30 02:36:15 +0100

    Add tool to check coverage of diff

--- test.py
+++ test.py
@@ -17,6 +17,11 @@ import logging
 import tests
 from tests import unittest
 
+try:
+	import coverage
+except ImportError:
+	coverage = None
+
 
 def main(argv=None):
 	'''Run either all tests, or those specified in argv'''
@@ -24,7 +29,7 @@ def main(argv=None):
 		argv = sys.argv
 
 	# parse options
-	coverage = None
+	covreport = False
 	failfast = False
 	loglevel = logging.WARNING
 	opts, args = getopt.gnu_getopt(argv[1:],
@@ -47,20 +52,14 @@ Options:
 ''' % argv[0]
 			return
 		elif o == '--coverage':
-			try:
-				import coverage as coverage_module
-			except ImportError:
+			if coverage:
+				covreport = True
+			else:
 				print >>sys.stderr, '''\
 Can not run test coverage without module 'coverage'.
 On Ubuntu or Debian install package 'python-coverage'.
 '''
 				sys.exit(1)
-			#~ coverage = coverage_module.coverage(data_suffix=True, auto_data=True)
-			coverage = coverage_module.coverage(data_suffix=True)
-			coverage.erase() # clean up old date set
-			coverage.exclude('assert ')
-			coverage.exclude('raise NotImplementedError')
-			coverage.start()
 		elif o == '--fast':
 			failfast = True
 			tests.FAST_TEST = True
@@ -73,6 +72,14 @@ On Ubuntu or Debian install package 'python-coverage'.
 			loglevel = logging.DEBUG
 		else:
 			assert False
+
+	# Start tracing
+	if coverage:			
+		cov = coverage.coverage(source=['zim'], branch=True)
+		cov.erase() # clean up old date set
+		cov.exclude('assert ')
+		cov.exclude('raise NotImplementedError')
+		cov.start()
 
 	# Set logging handler (don't use basicConfig here, we already installed stuff)
 	handler = logging.StreamHandler()
@@ -111,25 +118,17 @@ On Ubuntu or Debian install package 'python-coverage'.
 	test_report(result, 'test_report.html')
 	print '\nWrote test report to test_report.html\n'
 
-	# Create coverage output if asked to do so
+	# Stop tracing
 	if coverage:
-		coverage.stop()
-		#~ coverage.combine()
+		cov.stop()
+		cov.save()
 
+	# Create coverage output if asked to do so
+	if covreport:
 		print 'Writing coverage reports...'
-
-		pyfiles = list(tests.zim_pyfiles())
-		#~ coverage.report(pyfiles, show_missing=False)
-		#~ coverage.html_report(pyfiles, directory='./coverage', omit=['zim/inc/*'])
-		coverage_report(coverage, pyfiles, './coverage')
+		cov.html_report(directory='./coverage', omit=['zim/inc/*'])
 		print 'Done - Coverage reports can be found in ./coverage/'
 
-
-
-
-## #################################### ##
-## Functions to produce various reports ##
-## #################################### ##
 
 def test_report(result, file):
 	'''Produce html report of test failures'''
@@ -168,148 +167,6 @@ def test_report(result, file):
 	add_errors('FAIL', result.failures)
 
 	output.close()
-
-
-def coverage_report(coverage, pyfiles, directory):
-	'''Produce annotated text and html reports.
-	Alternative for coverage.html_report().
-	'''
-	if os.path.exists(directory):
-		shutil.rmtree(directory) # cleanup
-	os.mkdir(directory)
-
-	# reports per source file
-	index = []
-	for path in pyfiles:
-		if any(n in path for n in ('inc', '_version', '__main__')):
-			continue
-
-		txtfile = path[:-3].replace('/', '.') + '.txt'
-		htmlfile = path[:-3].replace('/', '.') + '.html'
-
-		p, statements, excluded, missing, l = coverage.analysis2(path)
-		nstat = len(statements)
-		nexec = nstat - len(missing)
-		index.append((path, htmlfile, nstat, nexec))
-
-		write_coverage_txt(path, directory+'/'+txtfile, missing, excluded, statements)
-		write_coverage_html(path, directory+'/'+htmlfile, missing, excluded, statements)
-
-	# Index for detailed reports
-	html = open(directory + '/index.html', 'w')
-	html.write('''\
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Test Coverage Index</title>
-<style>
-	.good    { background-color: #9f9; text-align: right }
-	.close   { background-color: #cf9; text-align: right }
-	.ontrack { background-color: #ff9; text-align: right }
-	.lacking { background-color: #fc9; text-align: right }
-	.bad     { background-color: #f99; text-align: right }
-	.int     { text-align: right }
-</style>
-</head>
-<body>
-<h1>Test Coverage Index</h1>
-<table>
-<tr><td><b>File</b></td><td><b>Stmts</b></td><td><b>Exec</b></td><td><b>Cover</b></td></tr>
-''')
-
-	total_stat = reduce(int.__add__, [r[2] for r in index])
-	total_exec = reduce(int.__add__, [r[3] for r in index])
-	total_perc = int( float(total_exec) / total_stat * 100 )
-	if total_perc >= 90: type = 'good'
-	elif total_perc >= 80: type = 'close'
-	elif total_perc >= 60: type = 'ontrack'
-	elif total_perc >= 40: type = 'lacking'
-	else: type = 'bad'
-	html.write('<tr><td><b>Total</b></td>'
-		       '<td class="int">%i</td><td class="int">%i</td>'
-			   '<td class="%s">%.0f%%</td></tr>\n'
-				   % (total_stat, total_exec, type, total_perc) )
-
-	for report in index:
-		pyfile, htmlfile, statements, executed = report
-		if statements: percentage = int( float(executed) / statements * 100 )
-		else: percentage = 100
-		if percentage >= 90: type = 'good'
-		elif percentage >= 80: type = 'close'
-		elif percentage >= 60: type = 'ontrack'
-		elif percentage >= 40: type = 'lacking'
-		else: type = 'bad'
-		html.write('<tr><td><a href="%s">%s</a></td>'
-		           '<td class="int">%i</td><td class="int">%i</td>'
-				   '<td class="%s">%.0f%%</td></tr>\n'
-				   % (htmlfile, pyfile, statements, executed, type, percentage) )
-	html.write('''\
-</table>
-</body>
-</html>
-''')
-	html.close()
-
-
-def write_coverage_txt(sourcefile, txtfile, missing, excluded, statements):
-	txt = open(txtfile, 'w')
-	file = open(sourcefile)
-	i = 0
-	for line in file:
-		i += 1
-		if   i in missing: prefix = '!'
-		elif i in excluded: prefix = '.'
-		elif i in statements: prefix = ' '
-		else: prefix = ' '
-		txt.write(prefix + line)
-	txt.close()
-
-
-def write_coverage_html(sourcefile, htmlfile, missing, excluded, statements):
-	html = open(htmlfile, 'w')
-	html.write('''\
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Coverage report for %s</title>
-<style>
-	.code { white-space: pre; font-family: monospace }
-	.executed { background-color: #9f9 }
-	.excluded { background-color: #ccc }
-	.missing  { background-color: #f99 }
-	.comment  { }
-</style>
-</head>
-<body>
-<h1>Coverage report for %s</h1>
-<table width="100%%">
-<tr><td class="executed">&nbsp;</td><td>Executed statement</td></tr>
-<tr><td class="missing">&nbsp;</td><td>Untested statement</td></tr>
-<tr><td class="excluded">&nbsp;</td><td>Ignored statement</td></tr>
-<tr><td>&nbsp</td><td>&nbsp</td></tr>
-''' % (sourcefile, sourcefile))
-
-	file = open(sourcefile)
-	i = 0
-	for line in file:
-		i += 1
-		if   i in missing: type = 'missing'
-		elif i in excluded: type = 'excluded'
-		elif i in statements: type = 'executed'
-		else: type = 'comment'
-
-		line = line.rstrip().replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
-		html.write('<tr><td class="%s">%i</td><td class="code">%s</td></tr>\n'
-						% (type, i, line) )
-	html.write('''\
-</table>
-</body>
-</html>
-''')
-	html.close()
-
-
-
 
 
 if __name__ == '__main__':
