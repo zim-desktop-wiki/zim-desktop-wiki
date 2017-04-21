c49eafe1e3d81f0f792a9d80444eb045a61de865
Jaap Karssenberg <jaap.karssenberg@gmail.com>
2017-04-21 18:20:54 +0200

    Fix for unicode links in table - Sašo Živanović

--- zim/plugins/tableeditor.py
+++ zim/plugins/tableeditor.py
@@ -741,7 +741,7 @@ class TableViewWidget(CustomObjectWidget):
 			cellvalue = self.fetch_cell_by_event(event, treeview)
 			linkvalue = self.get_linkurl(cellvalue)
 			if linkvalue:
-				self.emit('link-clicked', {'href': linkvalue})
+				self.emit('link-clicked', {'href': unicode(linkvalue)})
 			return
 
 		if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
@@ -846,7 +846,7 @@ class TableViewWidget(CustomObjectWidget):
 
 	def on_open_link(self, action, link):
 		''' Context menu: Open a link, which is written in a cell '''
-		self.emit('link-clicked', {'href': link})
+		self.emit('link-clicked', {'href': unicode(link)})
 
 	def on_open_help(self, action):
 		''' Context menu: Open help '''
