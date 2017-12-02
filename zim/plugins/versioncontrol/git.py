033d47a13de7455bc63de2a9d2129d5435a94d0f
Paul Becker <raincomplex@gmail.com>
2017-12-02 12:24:59 -0500

    versioncontrol improvements for git staging
    Change stage() to mean "prepare the repo for a commit". It is called by SaveVersionDialog before getting the status of the repo. It is only implemented for git, where it does a 'git add' on the whole notebook, so that the status message accurately reflects what will be committed.
    
    Remove '-a' from 'git commit'. Both stage() and VCSBackend.commit() call add() before commit(). Having '-a' here might capture unwanted changes in a zim commit, if the notebook is part of larger repo.
    
    Remove use_staging. It was only used with git and the new way of staging means it isn't necessary.
    
    Fixes #249
    Fixes #15
    Closes #250

--- zim/plugins/versioncontrol/git.py
+++ zim/plugins/versioncontrol/git.py
@@ -16,27 +16,7 @@ from zim.applications import Application
 logger = logging.getLogger('zim.vcs.git')
 
 
-# NOTE about staging: git has the concept of an "index" or "staging area"
-# that captures changes for the next commit and which is seperate from the
-# changes in the working directory. Specifically in contrast to the other
-# vcs systems supported in zim, git "add" only adds changes at that time, not
-# the files itself.
-#
-# Two operation modes:
-# 1. commit done through zim, we can run "add" on commit time and capture all
-#    changes, but also e.g. attachments added via the file manager
-# 2. commit is done outside of zim, we can run "add" on page and file changes
-#    to ensure latest version is captured by git commit. We do not cover files
-#    added e.g. via file manager in this case
-#
-# Thus we need to update staging on each page and file change, as well as
-# look for new files on commit.
-#
-
-
 class GITApplicationBackend(VCSApplicationBase):
-
-	use_staging = True
 
 	@classmethod
 	def build_bin_application_instance(cls):
@@ -111,10 +91,10 @@ class GITApplicationBackend(VCSApplicationBase):
 
 	def commit(self, path, msg):
 		"""
-		Runs: git commit -a -m {{MSG}} {{PATH}}
+		Runs: git commit -m {{MSG}} {{PATH}}
 		"""
 		if self.is_modified():
-			params = ['commit', '-a']
+			params = ['commit']
 			if msg != '' and msg is not None:
 				params.append('-m')
 				params.append(msg)
@@ -247,8 +227,8 @@ class GITApplicationBackend(VCSApplicationBase):
 		else:
 			self.run(['reset', '--hard', 'HEAD'])
 
-	def stage(self, file):
-		self.run(['add', file.relpath(self.root)])
+	def stage(self):
+		self.add()
 
 	def status(self, porcelain=False):
 		"""
