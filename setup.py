e46a4b40a90d6903b6fa860bd72a4464dab7d704
Jaap Karssenberg <jaap.karssenberg@gmail.com>
2018-01-08 21:09:18 +0100

    Release 0.68~rc1

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ def collect_data_files():
 		('share/applications', ['xdg/zim.desktop']),
 		('share/mime/packages', ['xdg/zim.xml']),
 		('share/pixmaps', ['xdg/hicolor/48x48/apps/zim.png']),
-		('share/appdata', ['xdg/zim.appdata.xml']),
+		('share/metainfo', ['xdg/org.zim-wiki.Zim.metainfo.xml']),
 	]
 
 	# xdg/hicolor -> PREFIX/share/icons/hicolor
--- zim/__init__.py
+++ zim/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2008-2017 Jaap Karssenberg <jaap.karssenberg@gmail.com>
+# Copyright 2008-2018 Jaap Karssenberg <jaap.karssenberg@gmail.com>
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -96,10 +96,10 @@ Some generic base classes and functions can be found in L{zim.utils}
 
 
 # Bunch of meta data, used at least in the about dialog
-__version__ = '0.67'
+__version__ = '0.68-rc1'
 __url__ = 'http://www.zim-wiki.org'
 __author__ = 'Jaap Karssenberg <jaap.karssenberg@gmail.com>'
-__copyright__ = 'Copyright 2008 - 2017 Jaap Karssenberg <jaap.karssenberg@gmail.com>'
+__copyright__ = 'Copyright 2008 - 2018 Jaap Karssenberg <jaap.karssenberg@gmail.com>'
 __license__ = '''\
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
