zim-desktop-wiki/zim-desktop-wiki

commit e91751f71b63fde8dc1ba17347eb8a099ac95d6a

Fix regression with startup in new environment
Ensure dialogs to prompt for new notebook
run before application window is started.

Closes #358

zim/gui/notebookdialog.py
@@ -35,6 +35,14 @@ PIXBUF_COL = 3 # column containing the notebook icon
 INFO_COL = 4   # column with the NotebookInfo object
 
 
+def _run_dialog_with_mainloop(dialog):
+	# Apparently Gtk.Dialog.run() does not work outside of a main loop
+	dialog.show_all()
+	dialog.present()
+	dialog.connect("response", lambda *a: Gtk.main_quit())
+	Gtk.main()
+	return dialog.result
+
 def prompt_notebook():
 	'''Prompts the NotebookDialog and returns the result or None.
 	As a special case for first time usage it immediately prompts for
@@ -44,7 +52,7 @@ def prompt_notebook():
 	list = get_notebook_list()
 	if len(list) == 0:
 		logger.debug('First time usage - prompt for notebook folder')
-		fields = AddNotebookDialog(None).run()
+		fields = _run_dialog_with_mainloop(AddNotebookDialog(None))
 		if fields:
 			dir = Dir(fields['folder'])
 			init_notebook(dir, name=fields['name'])
@@ -55,7 +63,7 @@ def prompt_notebook():
 			return None # User canceled the dialog ?
 	else:
 		# Multiple notebooks defined and no default
-		return NotebookDialog(None).run()
+		return _run_dialog_with_mainloop(NotebookDialog(None))
 
 
 class NotebookTreeModel(Gtk.ListStore):

tools/zim_tmp_env.sh
@@ -1,0 +1,22 @@
+#!/bin/sh
+
+# Run zim.py but with a temporary environment
+# allows testing startup without config files
+
+export TMPDIR="/tmp/zim-tmp-env"
+
+echo "Setting up environment in:" $TMPDIR
+
+rm -fr $TMPDIR
+mkdir $TMPDIR
+
+mkdir $TMPDIR/home
+
+export HOME=$TMPDIR/home
+export XDG_DATA_HOME=$TMPDIR/xdg_data_home
+#export XDG_DATA_DIRS=$TMPDIR/xdg_data_dir
+export XDG_CONFIG_HOME=$TMPDIR/xdg_config_home
+export XDG_CONFIG_DIRS=$TMPDIR/xdg_config_dir
+export XDG_CACHE_HOME=$TMPDIR/xdg_cache_home
+
+./zim.py $@

zim/gui/__init__.py
@@ -23,18 +23,19 @@ def load_zim_stock_icons():
 	factory = Gtk.IconFactory()
 	factory.add_default()
 	for dir in data_dirs(('pixmaps')):
-		for file in dir.list('*.png'):
+		for basename in dir.list('*.png'):
 			# not all installs have svg support, so only check png for now..
-			name = 'zim-' + file[:-4] # e.g. checked-box.png -> zim-checked-box
+			name = 'zim-' + basename[:-4] # e.g. checked-box.png -> zim-checked-box
 			icon_theme = Gtk.IconTheme.get_default()
 			try:
-			    pixbuf = icon_theme.load_icon(name, 24, 0)
+				pixbuf = icon_theme.load_icon(name, 24, 0)
 			except:
-			    pixbuf = GdkPixbuf.Pixbuf.new_from_file(str(dir + file))
+				path = dir.file(basename).path
+				pixbuf = GdkPixbuf.Pixbuf.new_from_file(path)
 
 			try:
-			    set = Gtk.IconSet(pixbuf)
-			    factory.add(name, set)
+				set = Gtk.IconSet(pixbuf)
+				factory.add(name, set)
 			except Exception:
 				logger.exception('Got exception while loading application icons')
 

commit 05102b46607775b4c5f7e97d599eaa143042d1f7

Fix test case for notebookdialog
Fix regression introduced in:
e91751f71b63fde8dc1ba17347eb8a099ac95d6a

zim/gui/notebookdialog.py
@@ -37,10 +37,14 @@ INFO_COL = 4   # column with the NotebookInfo object
 
 def _run_dialog_with_mainloop(dialog):
 	# Apparently Gtk.Dialog.run() does not work outside of a main loop
-	dialog.show_all()
-	dialog.present()
-	dialog.connect("response", lambda *a: Gtk.main_quit())
-	Gtk.main()
+	from zim.gui.widgets import TEST_MODE, TEST_MODE_RUN_CB
+	if TEST_MODE and TEST_MODE_RUN_CB:
+		TEST_MODE_RUN_CB(dialog)
+	else:
+		dialog.show_all()
+		dialog.present()
+		dialog.connect("response", lambda *a: Gtk.main_quit())
+		Gtk.main()
 	return dialog.result
 
 def prompt_notebook():