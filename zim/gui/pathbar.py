zim-desktop-wiki/zim-desktop-wiki

commit 505bee194021ed3a0b8cd1c84ed754c4f3fe08a4

Show unique names in pathbar
Fix algorithm for unique names in pathbar, now shows
arbitrary number of elements to make names unique.

Closes #364

zim/gui/pathbar.py
@@ -7,12 +7,12 @@ import gtk
 import gobject
 import logging
 
-from collections import Counter
-
 from zim.gui.clipboard import \
 	INTERNAL_PAGELIST_TARGET_NAME, INTERNAL_PAGELIST_TARGET, \
 	pack_urilist
 from zim.gui.widgets import encode_markup_text
+from zim.notebook.page import shortest_unique_names
+
 from functools import reduce
 
 logger = logging.getLogger('zim.gui')
@@ -389,8 +389,6 @@ class ScrollButton(gtk.Button):
 		self.add(gtk.Arrow(arrow_dir, gtk.SHADOW_OUT))
 		self.set_relief(gtk.RELIEF_NONE)
 
-def _last_two_segments(name):
-	return ":".join(name.split(':')[-2:]);
 
 class PathBar(ScrolledHBox):
 	'''Base class for pathbars in the zim GUI, extends ScrolledHBox for usage
@@ -427,15 +425,8 @@ class PathBar(ScrolledHBox):
 			self.remove(button)
 		self._selected = None
 
-		basenames = list(map(lambda x: x.basename, self.get_paths()))
-		basenameCounts = Counter(basenames);
-
-		for path in self.get_paths():
-			if basenameCounts[path.basename] > 1 :
-				label = _last_two_segments(path.name)
-			else:
-				label = path.basename
-
+		paths = self.get_paths()
+		for path, label in zip(paths, shortest_unique_names(paths)):
 			button = gtk.ToggleButton(label=label)
 			button.set_use_underline(False)
 			button.zim_path = path

tests/notebook.py
@@ -792,6 +792,31 @@ class TestPath(tests.TestCase):
 	# TODO test operators on paths > < + - >= <= == !=
 
 
+class TestShortestUniqueNames(tests.TestCase):
+
+	def runTest(self):
+		from zim.notebook.page import shortest_unique_names
+		paths = [
+			Path('Test'),
+			Path('Foo'),
+			Path('2017:03:01'),
+			Path('2018:03:01'),
+			Path('2018:02:01'),
+			Path('Foo:Bar'),
+			Path('Dus:Foo')
+		]
+		wanted = [
+			'Test',
+			'Foo',
+			'2017:03:01',
+			'2018:03:01',
+			'02:01',
+			'Bar',
+			'Dus:Foo'
+		]
+		self.assertEqual(shortest_unique_names(paths), wanted)
+
+
 class TestHRefFromWikiLink(tests.TestCase):
 
 	def runTest(self):

zim/notebook/page.py
@@ -5,6 +5,7 @@
 
 import re
 import logging
+import itertools
 
 logger = logging.getLogger('zim.notebook')
 
@@ -35,6 +36,42 @@ re.UNICODE)
 	# This pattern matches a non-alphanumber at start or after the ':'
 	# seperator. It also matches any invalid character.
 	# The UNICODE flag is used to make the alphanumber check international.
+
+
+def shortest_unique_names(paths):
+	'''Returns the shortest unique name for each path in paths
+	@param paths: list of L{Path} objects
+	@returns: list of strings
+	'''
+	by_basename = {}
+	for path in paths:
+		basename = path.basename
+		mylist = by_basename.setdefault(basename, [])
+		mylist.append(path)
+
+	result = []
+	for path in paths:
+		basename = path.basename
+		conflicts = by_basename[basename]
+		if len(conflicts) == 1:
+			result.append(path.basename)
+		else:
+			conflicts.remove(path)
+			conflicts.insert(0, path) # shuffle path of interest to front
+			reverse_paths = [reversed(p.name.split(':')) for p in conflicts]
+			names = []
+			for parts in itertools.izip_longest(*reverse_paths):
+				if parts[0] is None:
+					break
+				elif parts[0] not in parts[1:]:
+					names.append(parts[0])
+					break
+				else:
+					names.append(parts[0])
+
+			result.append(':'.join(reversed(names)))
+
+	return result
 
 
 class Path(object):

commit 3c4a1152d31b0c831b0dad1cd0eb97de131fba64

Fix regression in pathbar
Regression in commit 505bee194021ed3a0b8cd1c84ed754c4f3fe08a4
for issue #364

zim/gui/pathbar.py
@@ -425,7 +425,7 @@ class PathBar(ScrolledHBox):
 			self.remove(button)
 		self._selected = None
 
-		paths = self.get_paths()
+		paths = list(self.get_paths())
 		for path, label in zip(paths, shortest_unique_names(paths)):
 			button = gtk.ToggleButton(label=label)
 			button.set_use_underline(False)