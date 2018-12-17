zim-desktop-wiki/zim-desktop-wiki

commit 3bb2c4927e119f200887005e5ac4759d4ff0ae2c

Future proof API functions that return additional boolean
A few functions in the Gtk API return an additional booelan
value. Looks like the "strip_boolean_result" wrapper is
missing. Made our own wrapper to ensure we are robust if
this API turns out to be fixed in some other version of the
language bindings.

zim/gui/pageview.py
@@ -43,7 +43,7 @@ from zim.gui.widgets import \
 	Dialog, FileDialog, QuestionDialog, ErrorDialog, \
 	IconButton, MenuButton, BrowserTreeView, InputEntry, \
 	ScrolledWindow, \
-	rotate_pixbuf, populate_popup_add_separator
+	rotate_pixbuf, populate_popup_add_separator, strip_boolean_result
 from zim.gui.applications import OpenWithMenu, open_url, open_file, edit_config_file
 from zim.gui.clipboard import Clipboard, SelectionClipboard, \
 	textbuffer_register_serialize_formats
@@ -3658,7 +3658,7 @@ class TextView(Gtk.TextView):
 			mark = buffer.create_mark('zim-popup-menu', iter, True)
 
 	def do_key_press_event(self, event):
-		ok, keyval = event.get_keyval()
+		keyval = strip_boolean_result(event.get_keyval())
 		#print 'KEY %s (%r)' % (Gdk.keyval_name(keyval), keyval)
 		event_state = event.get_state()
 		#print 'STATE %s' % event_state
@@ -3957,7 +3957,7 @@ class TextView(Gtk.TextView):
 		'''
 		x, y = self.get_pointer()
 		x, y = self.window_to_buffer_coords(Gtk.TextWindowType.WIDGET, x, y)
-		ok, iter = self.get_iter_at_location(x, y)
+		iter = strip_boolean_result(self.get_iter_at_location(x, y))
 		return iter, (x, y)
 
 	def _get_pixbuf_at_pointer(self, iter, coords):
@@ -4001,7 +4001,7 @@ class TextView(Gtk.TextView):
 		if coords is None:
 			iter, coords = self._get_pointer_location()
 		else:
-			ok, iter = self.get_iter_at_location(*coords)
+			iter = strip_boolean_result(self.get_iter_at_location(*coords))
 
 		link = None
 		pixbuf = self._get_pixbuf_at_pointer(iter, coords)
@@ -7177,7 +7177,7 @@ class FindBar(FindWidget, Gtk.HBox):
 		self.textview.grab_focus()
 
 	def do_key_press_event(self, event):
-		ok, keyval = event.get_keyval()
+		keyval = strip_boolean_result(event.get_keyval())
 		if keyval == KEYVAL_ESC:
 			self.hide()
 			return True

zim/gui/applications.py
@@ -26,7 +26,7 @@ from zim.config import XDG_DATA_HOME, XDG_DATA_DIRS, XDG_CONFIG_HOME, \
 	data_dirs, SectionedConfigDict, INIConfigFile, json, ConfigManager
 from zim.parsing import split_quoted_strings, uri_scheme
 from zim.applications import Application, WebBrowser, StartFile
-from zim.gui.widgets import Dialog, ErrorDialog, MessageDialog
+from zim.gui.widgets import Dialog, ErrorDialog, MessageDialog, strip_boolean_result
 
 
 logger = logging.getLogger('zim.gui.applications')
@@ -798,7 +798,7 @@ class DesktopEntryDict(SectionedConfigDict, Application):
 		if isinstance(icon, File):
 			icon = icon.path
 
-		ok, w, h = Gtk.icon_size_lookup(size)
+		w, h = strip_boolean_result(Gtk.icon_size_lookup(size))
 
 		if '/' in icon or '\\' in icon:
 			if zim.fs.isfile(icon):

zim/gui/notebookdialog.py
@@ -23,7 +23,8 @@ import zim.main
 from zim.fs import File, Dir
 from zim.notebook import get_notebook_list, get_notebook_info, init_notebook, NotebookInfo
 from zim.config import data_file
-from zim.gui.widgets import Dialog, IconButton, encode_markup_text, ScrolledWindow
+from zim.gui.widgets import Dialog, IconButton, encode_markup_text, ScrolledWindow, \
+	strip_boolean_result
 
 logger = logging.getLogger('zim.gui.notebookdialog')
 
@@ -130,7 +131,7 @@ class NotebookTreeModel(Gtk.ListStore):
 				# T: Path label in 'open notebook' dialog
 
 		if info.icon and File(info.icon).exists():
-			ok, w, h = Gtk.icon_size_lookup(Gtk.IconSize.BUTTON)
+			w, h = strip_boolean_result(Gtk.icon_size_lookup(Gtk.IconSize.BUTTON))
 			pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(File(info.icon).path, w, h)
 		else:
 			pixbuf = None
@@ -180,7 +181,7 @@ class NotebookTreeView(Gtk.TreeView):
 		cell_renderer = Gtk.CellRendererPixbuf()
 		column = Gtk.TreeViewColumn(None, cell_renderer, pixbuf=PIXBUF_COL)
 		column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
-		ok, w, h = Gtk.icon_size_lookup(Gtk.IconSize.MENU)
+		w, h = strip_boolean_result(Gtk.icon_size_lookup(Gtk.IconSize.MENU))
 		column.set_fixed_width(w * 2)
 		self.append_column(column)
 

zim/gui/widgets.py
@@ -104,6 +104,23 @@ def decode_markup_text(text):
 	return text.replace('&gt;', '>').replace('&lt;', '<').replace('&amp;', '&')
 
 
+def strip_boolean_result(ret):
+	# Wrapper to remove additional boolean
+	# See also gi.overrides strip_boolean_result()
+	# This version also double checks whether result was stripped already
+	# to be robust for API changes
+	if ret.__class__.__name__ == '_ResultTuple':
+		if ret[0]:
+			if len(ret) == 2:
+				return ret[1]
+			else:
+				return ret[1:]
+		else:
+			return None
+	else:
+		return ret
+
+
 def gtk_window_set_default_icon():
 	'''Function to set the zim icon as the default window icon for
 	all gtk windows in this process.

commit 5e2601d549eaac3b648f23bb8dccba8144cb3478

Fix regression in pageview
Regression introduced in 3bb2c4927e119f200887005e5ac4759d4ff0ae2c
by more strict checking of return boolean.

zim/gui/pageview.py
@@ -3604,11 +3604,12 @@ class TextView(Gtk.TextView):
 
 		if event.type == Gdk.EventType.BUTTON_PRESS:
 			iter, coords = self._get_pointer_location()
-			if event.button == 2 and not buffer.get_has_selection():
-				buffer.paste_clipboard(SelectionClipboard, iter, self.get_editable())
-				return False
-			elif event.button == 3:
-				self._set_popup_menu_mark(iter)
+			if iter:
+				if event.button == 2 and not buffer.get_has_selection():
+					buffer.paste_clipboard(SelectionClipboard, iter, self.get_editable())
+					return False
+				elif event.button == 3:
+					self._set_popup_menu_mark(iter)
 
 		return Gtk.TextView.do_button_press_event(self, event)
 
@@ -4003,24 +4004,26 @@ class TextView(Gtk.TextView):
 		else:
 			iter = strip_boolean_result(self.get_iter_at_location(*coords))
 
-		link = None
-		pixbuf = self._get_pixbuf_at_pointer(iter, coords)
-		if pixbuf:
-			if pixbuf.zim_type == 'icon' and pixbuf.zim_attrib['stock'] in (
-				STOCK_CHECKED_BOX, STOCK_UNCHECKED_BOX, STOCK_XCHECKED_BOX, STOCK_MIGRATED_BOX):
-				cursor = CURSOR_WIDGET
-			elif 'href' in pixbuf.zim_attrib:
-				link = {'href': pixbuf.zim_attrib['href']}
-				cursor = CURSOR_LINK
-			else:
-				cursor = CURSOR_TEXT
+		if iter is None:
+			self._set_cursor(CURSOR_TEXT)
 		else:
-			link = self.get_buffer().get_link_data(iter)
-			if link:
-				cursor = CURSOR_LINK
+			pixbuf = self._get_pixbuf_at_pointer(iter, coords)
+			if pixbuf:
+				if pixbuf.zim_type == 'icon' and pixbuf.zim_attrib['stock'] in (
+					STOCK_CHECKED_BOX, STOCK_UNCHECKED_BOX, STOCK_XCHECKED_BOX, STOCK_MIGRATED_BOX):
+					self._set_cursor(CURSOR_WIDGET)
+				elif 'href' in pixbuf.zim_attrib:
+					self._set_cursor(CURSOR_LINK, link={'href': pixbuf.zim_attrib['href']})
+				else:
+					self._set_cursor(CURSOR_TEXT)
 			else:
-				cursor = CURSOR_TEXT
+				link = self.get_buffer().get_link_data(iter)
+				if link:
+					self._set_cursor(CURSOR_LINK, link=link)
+				else:
+					self._set_cursor(CURSOR_TEXT)
 
+	def _set_cursor(self, cursor, link=None):
 		if cursor != self._cursor:
 			window = self.get_window(Gtk.TextWindowType.TEXT)
 			window.set_cursor(cursor)
@@ -4047,11 +4050,14 @@ class TextView(Gtk.TextView):
 		@returns: C{True} when there was indeed a link
 		'''
 		iter, coords = self._get_pointer_location()
+		if iter is None:
+			return False
+	
 		pixbuf = self._get_pixbuf_at_pointer(iter, coords)
 		if pixbuf and pixbuf.zim_attrib.get('href'):
 			self.emit('link-clicked', {'href': pixbuf.zim_attrib['href']})
 			return True
-		else:
+		elif iter:
 			return self.click_link_at_iter(iter)
 
 	def click_link_at_iter(self, iter):
@@ -4079,7 +4085,7 @@ class TextView(Gtk.TextView):
 		@returns: C{True} for success, C{False} if no checkbox was found.
 		'''
 		iter, coords = self._get_pointer_location()
-		if iter.get_line_offset() < 2:
+		if iter and iter.get_line_offset() < 2:
 			# Only position 0 or 1 can map to a checkbox
 			buffer = self.get_buffer()
 			recurs = self.preferences['recursive_checklist']