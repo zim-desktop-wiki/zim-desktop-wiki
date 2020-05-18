000399c201bbf3098349d5a02aec169e3e3fb7a5
bytebieger <bonk_1312@web.de>
2020-05-19 00:04:10 +0200

    Test cases for #1053
    handling of non-existent pages #1053

--- tests/notebook.py
+++ tests/notebook.py
@@ -600,6 +600,31 @@ class TestUpdateLinksOnMovePage(tests.TestCase):
 		self.assertEqual(self.getNotebookContent(notebook), post[0])
 		self.assertEqual(self.getNotebookLinks(notebook), set(post[1]))
 
+	def testMoveNonPageLink(self):
+		self.movePage(
+			pre=(
+				{'A': 'test 123\n', 'B': '[[C]]\n'},
+				[('B', 'C')]
+			),
+			move=('C', 'A'),
+			post=(
+				{'A': 'test 123\n', 'B': '[[A]]\n'},
+				[('B', 'A')]
+			)
+		)
+
+	def testRenameNonPageLink(self):
+		self.movePage(
+			pre=(
+				{'A': 'test 123\n', 'B': '[[C]]\n'},
+				[('B', 'C')]
+			),
+			move=('C', 'D'),
+			post=(
+				{'A': 'test 123\n', 'B': '[[D]]\n', 'D': ''},
+				[('B', 'D')]
+			)
+		)
 	def testFloatingLink(self):
 		self.movePage(
 			pre=(
