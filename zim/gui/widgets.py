162ea04f64e98337c703f31318aa2619fe3fc8be
Jaap Karssenberg <jaap.karssenberg@gmail.com>
2023-07-02 15:45:28 +0200

    fixup simplify patch

--- zim/gui/widgets.py
+++ zim/gui/widgets.py
@@ -1685,6 +1685,19 @@ class FolderEntry(FSPathEntry):
 	set_folder = FSPathEntry.set_path
 	get_folder = FSPathEntry.get_path
 
+
+def gtk_entry_completion_match_func_words(completion, key, iter, column):
+	if key is None:
+		return False
+
+	model = completion.get_model()
+	text = to_utf8_normalized_casefolded(model.get_value(iter, column))
+	if text is not None:
+		return (key in text) or any(w in text for w in key.split())
+	else:
+		return False
+
+
 def gtk_entry_completion_match_func_startswith(completion, key, iter, column):
 	if key is None:
 		return False
@@ -1896,6 +1909,7 @@ class PageEntry(InputEntry):
 	def _fill_completion_for_anchor(self, path, prefix, text):
 		#print "COMPLETE ANCHOR", path, prefix, text
 		# Complete a single namespace based on the prefix
+		#
 		# TODO: allow filter on "text" directly in SQL call
 		completion = self.get_completion()
 		completion.set_match_func(gtk_entry_completion_match_func_startswith, 1)
@@ -1912,29 +1926,16 @@ class PageEntry(InputEntry):
 	def _fill_completion_any(self, path, text):
 		# Complete all matches of "text"
 		# start with children and peers, than peers of parents, than rest of tree
-
-		completion = self.get_completion()
-
-		# determine if a row should or should not be in the completion list.
-		def gtk_entry_completion_match_func(_completion, _input, _iter, column):
-			if _input is None:
-				return False
-
-			_model = _completion.get_model()
-			_page_name = to_utf8_normalized_casefolded(_model.get_value(_iter, column))
-
-			if _page_name is not None and _input in _page_name:
-				return True
-
-			if _page_name is not None and any(_keywords in _page_name for _keywords in _input.split()):
-				return True
-
-			return False
-
-		completion.set_match_func(gtk_entry_completion_match_func, 1)
-
+		# and also look for matches where input is split in words,
+		# which can match at different offsets
+		#
 		# TODO: use SQL to list all at once instead of walking and filter on "text"
 		#       do better sorting as well ?
+
+		MAX = 20
+
+		completion = self.get_completion()
+		completion.set_match_func(gtk_entry_completion_match_func_words, 1)
 
 		if path.isroot:
 			def relative_link(target):
@@ -1949,26 +1950,27 @@ class PageEntry(InputEntry):
 		searchpath = list(path.parents())
 		searchpath.insert(1, path) # children after peers but before parents
 		for namespace in searchpath:
-			for p in self.notebook.pages.match_pages(namespace, text):
+			for p in self.notebook.pages.match_pages(namespace, text, limit=MAX):
 				link = relative_link(p)
 				completion_set.add((link, p.basename))
 
-			if len(completion_set) > 20:
+			if len(completion_set) > MAX:
 				break
 
-		if len(completion_set) < 20:
-			for p in self.notebook.pages.match_all_pages(text, limit=20):
+		if len(completion_set) < MAX:
+			for p in self.notebook.pages.match_all_pages(text, limit=MAX):
 				link = relative_link(p)
 				completion_set.add((link, p.basename))
 
-		if len(completion_set) < 20:
-			for p in self.notebook.pages.match_all_locations(text.split(), limit=20):
+		if len(completion_set) < MAX:
+			for p in self.notebook.pages.match_all_pages_by_words(text.split(), limit=MAX):
 				link = relative_link(p)
 				completion_set.add((link, p.basename))
 
 		model = completion.get_model()
 		for i in completion_set:
 			model.append(i)
+
 
 class NamespaceEntry(PageEntry):
 	'''Widget to select a zim page path as a namespace
--- zim/notebook/index/pages.py
+++ zim/notebook/index/pages.py
@@ -586,23 +586,17 @@ class PagesView(IndexView):
 			yield PageIndexRecord(row)
 
 
-	def match_all_locations(self, keywords: list, limit: int = 10) -> Generator[PageIndexRecord, None, None]:
-		'''Like C{match_all_pages()}, except it performs a fuzzy search'''
+	def match_all_pages_by_words(self, words: list, limit: int = 10) -> Generator[PageIndexRecord, None, None]:
+		'''Like C{match_all_pages()}, except it performs a search based on multiple words'''
 
-		query_fragments = []
+		query_fragments = ['SELECT * FROM pages WHERE 1']
 		query_parameters = []
 
-		query_fragments.append('SELECT * FROM pages')
-
-		query_fragments.append('WHERE 1')
-
-		for text in keywords:
+		for w in words:
 			query_fragments.append("AND name LIKE ?")
-			query_parameters.append("%%%s%%" % text.lower())
+			query_parameters.append("%%%s%%" % w.lower())
 
-		query_fragments.append('ORDER BY length(name), sortkey, name')
-
-		query_fragments.append('LIMIT ?')
+		query_fragments.append('ORDER BY length(name), sortkey, name LIMIT ?')
 		query_parameters.append(limit)
 
 		for row in self.db.execute(" ".join(query_fragments), query_parameters):
