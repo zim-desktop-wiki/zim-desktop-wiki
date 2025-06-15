b3f8615292fece0952db38fcdc796867fb3924a8
Bob Vloon <bobv@bananacorp.nl>
2025-06-15 12:21:39 +0200

    One should not commit files without temporary deleted code.

--- tests/quicknote.py
+++ tests/quicknote.py
@@ -15,7 +15,44 @@ from zim.gui.clipboard import Clipboard, SelectionClipboard
 @tests.skipIf(os.name == 'nt', 'QuickNote not supported on Windows')
 class TestQuickNotePlugin(tests.TestCase):
 
+	def assertRun(self, args, text):
+		# Bootstrap via "handle_local_commandline" to emulate
+		# how application will handle this
+		cmd = QuickNotePluginCommand('quicknote')
+		cmd.parse_options(*args)
+		args = cmd.handle_local_commandline(list(args))
+
+		cmd = QuickNotePluginCommand('quicknote')
+		cmd.parse_options(*args)
+		dialog = cmd.run()
+
+		self.assertIsInstance(dialog, QuickNoteDialog)
+		buffer = dialog.textview.get_buffer()
+		start, end = buffer.get_bounds()
+		result = start.get_text(end)
+		self.assertIn(text, result)
+
 	def testMain(self):
+		# Text on commandline
+		text = 'foo bar baz\ndus 123'
+		self.assertRun(('--text', text), text)
+
+		encoded = 'Zm9vIGJhciBiYXoKZHVzIDEyMwo='
+		self.assertRun(('--text', encoded, '--encoding', 'base64'), text)
+
+		encoded = 'foo%20bar%20baz%0Adus%20123'
+		self.assertRun(('--text', encoded, '--encoding', 'url'), text)
+
+		# Clipboard input
+		text = 'foo bar baz\ndus 123'
+		SelectionClipboard.clipboard.clear() # just to be sure
+		SelectionClipboard.clipboard.set_text('', -1) # HACK to clear it
+		Clipboard.set_text(text)
+		self.assertRun(('--input', 'clipboard',), text)
+
+		text = 'foo bar baz\ndus 456'
+		SelectionClipboard.set_text(text)
+		self.assertRun(('--input', 'clipboard',), text)
 
 		# Template options
 		cmd = QuickNotePluginCommand('quicknote')
